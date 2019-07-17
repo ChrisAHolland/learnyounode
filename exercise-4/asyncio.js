4f897abd54eaa2e43a347bc7825031731c03dd19
Chris Holland <chris.holland@aot-technologies.com>
2019-07-17 14:39:19 -0700

    Added error catch and ex 5 directory

--- exercise-4/asyncio.js
+++ exercise-4/asyncio.js
@@ -4,6 +4,7 @@ let newlines = 0;
 
 function countNewlines(callback) {
     fs.readFile(file, 'utf8', function doneReading(err, fileContents) {
+        if (err) return console.error(err)
         newlines = fileContents.split('\n').length-1
         callback()
     })

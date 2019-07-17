4c6216020eb01d4c6b0258c91ed119e08d9bce0d
Chris Holland <christopherholland@uvic.ca>
2019-07-16 21:02:17 -0700

    Exercise 3 Betterment

--- exercise-3/firstio.js
+++ exercise-3/firstio.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
 
-var path = process.argv[2]
-var text = fs.readFileSync(path).toString()
-var lines = text.split('\n')
-var newlines = lines.length - 1
+const text = fs.readFileSync(process.argv[2], 'utf8')
+const newlines = text.split('\n').length - 1
 console.log(newlines)

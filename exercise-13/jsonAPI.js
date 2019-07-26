169b153e0f200a87c234f5bd8188784cb4f36113
Chris Holland <christopherholland@uvic.ca>
2019-07-25 19:37:31 -0700

    Final Challenge Complete!

--- exercise-13/jsonAPI.js
+++ exercise-13/jsonAPI.js
@@ -2,8 +2,40 @@ const http = require('http')
 const url = require('url')
 const port = process.argv[2]
 
-const server = http.createServer(function(req, res) {
+//Function to get the time in a a parsed format
+function parseTime(time) {
+    return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+    }
+}
 
+//Function to get the time in Unix format
+function unixTime(time) {
+    return { unixtime: time.getTime() }
+}
+
+const server = http.createServer(function(req, res) {
+    const parsedUrl = url.parse(req.url, true)
+    const time = new Date(parsedUrl.query.iso)
+    let result
+
+    //RegEx the URL to see what format is desired
+    if (/^\/api\/parsetime/.test(req.url)) {
+        result = parseTime(time)
+    } else if (/^\/api\/unixtime/.test(req.url)) {
+        result = unixTime(time)
+    }
+
+    //Ensure the URL was valid
+    if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+    } else {
+        res.writeHead(404)
+        res.end()
+    }
 })
 
 server.listen(port)

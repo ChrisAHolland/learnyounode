a91fdd895570585b08f06cf2289e51fe9c86910e
Chris Holland <christopherholland@uvic.ca>
2019-08-01 21:22:05 -0700

    Formatting Change

--- exercise-12/httpUppercaser.js
+++ exercise-12/httpUppercaser.js
@@ -3,9 +3,7 @@ const map = require('through2-map')
 const port = process.argv[2]
 
 const server = http.createServer(function(req, res) {
-    if (req.method !== 'POST') {
-        return res.end()
-    }
+    if (req.method !== 'POST') return res.end()
     
     req.pipe(map(function(chunk) {
         return chunk.toString().toUpperCase()
